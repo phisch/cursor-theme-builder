c272fcfff4b596fa0886ef6a1b2a832273f97a3e
Philipp Schaffrath <philipp.schaffrath@gmail.com>
2022-09-20 00:12:02 +0200

    display example theme.json in editor

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as monaco from 'monaco-editor'
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
 import { cursorThemeSchema } from './json-schema/cursor-theme'
+import example_json from "../example/theme.json"
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
@@ -25,11 +26,12 @@ monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
   ]
 });
 
+const code = JSON.stringify(example_json, null, 2);
 const cursor_theme_editor = document.querySelector("#schema-validator .editor");
 if (cursor_theme_editor != null) {
   monaco.editor.create(cursor_theme_editor as HTMLElement, {
     language: 'json',
-    model: monaco.editor.createModel("", 'json'),
+    model: monaco.editor.createModel(code, 'json'),
     theme: "vs-dark",
     automaticLayout: true,
     tabSize: 2
